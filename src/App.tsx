3f6c4ec114e4bb865d4e97444470094fe0c2caf7
denizhankalkan <denizhankalkan@outlook.com>
2023-09-04 20:42:29 +0300

    Add sort function to recent trades

--- src/App.tsx
+++ src/App.tsx
@@ -1,90 +1,3 @@
-// import React, { useState, useEffect } from 'react';
-// import RecentTradesTable from './components/RecentTradesTable';
-// import axios from 'axios';
-
-// const App: React.FC = () => {
-//   const [pair, setPair] = useState<string>('btcusdt');
-//   const [ticker, setTicker] = useState<any>(null);
-//   const [trades, setTrades] = useState<any[]>([]);
-
-//   useEffect(() => {
-//     const wsTicker = new WebSocket(`wss://stream.binance.com:9443/ws/${pair}@ticker`);
-//     wsTicker.onmessage = (event) => {
-//       setTicker(JSON.parse(event.data));
-//     };
-
-//     axios.get(`https://api.binance.com/api/v3/trades?symbol=${pair.toUpperCase()}`)
-//       .then(response => {
-//         setTrades(response.data);
-//       })
-//       .catch(error => {
-//         console.error('Error fetching trade data:', error);
-//       });
-
-//     return () => {
-//       wsTicker.close();
-//     };
-//   }, [pair]);
-
-//   return (
-//     <div>
-//       <h1>Binance Market Data</h1>
-//       <form>
-//         <label>
-//           Select Trading Pair:
-//           <select value={pair} onChange={(e) => setPair(e.target.value)}>
-//             <option value="btcusdt">BTCUSDT</option>
-//             <option value="ethusdt">ETHUSDT</option>
-//             <option value="xrpusdt">XRPUSDT</option>
-//           </select>
-//         </label>
-//       </form>
-
-//       {ticker && <h2>Current Price: {ticker.c}</h2>}
-
-//       {trades.length > 0 && (
-//         <div>
-//           <h2>Recent Trades</h2>
-//           <RecentTradesTable trades={trades} />
-//         </div>
-//       )}
-//     </div>
-//   );
-// };
-
-// export default App;
-
-// import React from 'react';
-// import { useSelector, useDispatch } from 'react-redux';
-// import { setActiveTable } from './stores/actions';
-
-// function App() {
-//   const dispatch = useDispatch();
-//   const activeTable = useSelector((state: any) => state.table.activeTable);
-
-//   const handleTableChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
-//     dispatch(setActiveTable(e.target.value));
-//   };
-
-//   return (
-//     <div>
-
-//       <select onChange={handleTableChange}>
-//         <option value="TICKER">Ticker Data</option>
-//         <option value="TICKER_24H">24h Ticker Data</option>
-//         <option value="RECENT_TRADES">Recent Trades</option>
-//       </select>
-
-//       {activeTable === 'TICKER' && <div> tablo 1</div>}
-//       {activeTable === 'TICKER_24H' && <div> tablo 2 </div>}
-//       {activeTable === 'RECENT_TRADES' && <div>tablo 3</div>}
-//     </div>
-//   );
-// }
-
-// export default App;
-
-
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useSelector, useDispatch } from 'react-redux';
@@ -98,7 +11,7 @@ const App: React.FC = () => {
   const [currencyPair, setCurrencyPair] = useState('BTCUSDT');
   const [oneDayTrades, setOneDayTrades] = useState(null);
   const [recentTrades, setRecentTrades] = useState([]);
-  const [fixedRecentTrades, setFixedRecentTrades] = useState([]);
+  // const [tickerTrades, setTickerTrades] = useState([]);
 
     const dispatch = useDispatch();
     const activeTable = useSelector((state: any) => state.table.activeTable);
@@ -112,32 +25,24 @@ const App: React.FC = () => {
    
     ws.onmessage = (message) => {
       const data = JSON.parse(message.data);
-      setOneDayTrades(data);
+      console.log(data);
     };
-
-    // const wss = new WebSocket(`wss://stream.binance.com:9443/ws/${currencyPair.toLowerCase()}@ticker`);
    
-    // ws.onmessage = (message) => {
-    //   const data = JSON.parse(message.data);
-    //   setRecentTrades(data);
-    // };
-   
-    // Fetch initial data for recent trades
-    axios.get(`https://api.binance.com/api/v3/trades?symbol=${currencyPair}`)
+    axios.get(`https://api.binance.com/api/v3/trades?symbol=${currencyPair}&limit=20`)
       .then(response => setRecentTrades(response.data));
 
     axios.get(`https://api.binance.com/api/v3/ticker/24hr?symbol=${currencyPair}`)
-    .then(response => setFixedRecentTrades(response.data));
+      .then(response => setOneDayTrades(response.data));
 
-    // // Fetch initial data for fixed recent trades (BNBUSDT)
-    // axios.get('https://api.binance.com/api/v3/trades?symbol=BNBUSDT')
-    //   .then(response => setFixedRecentTrades(response.data));
+    // axios.get(`https://api.binance.com/api/v3/ticker/price?symbol=${currencyPair}`)
+    //   .then(response => setOneDayTrades(response.data));
 
     return () => {
       ws.close();
     };
   }, [currencyPair]);
 
+// {{url}}/api/v3/ticker/price  => our URL ??? NEED ASK 
   return (
     <div>
       <h1>Binance Market Data</h1>
@@ -151,15 +56,13 @@ const App: React.FC = () => {
       {/* <TickerTradesTable data={fixedRecentTrades} /> */}
 
     <select onChange={handleTableChange}>
-       <option value="TICKER">Ticker Data</option>
        <option value="TICKER_24H">24h Ticker Data</option>      
        <option value="RECENT_TRADES">Recent Trades</option>
     </select>
 
-     {activeTable === 'TICKER' && <div>  <OneDayTrades data={oneDayTrades} /> </div>}
-     {activeTable === 'TICKER_24H' && <div> <RecentTrades data={recentTrades} /></div>}
-     {activeTable === 'RECENT_TRADES' && <div>tablo 3</div>}
-
+     {activeTable === 'TICKER_24H' && <div>  <OneDayTrades data={oneDayTrades} /> </div>}
+     {activeTable === 'RECENT_TRADES' && <div> <RecentTrades data={recentTrades} /></div>}
+     {/* {activeTable === 'RECENT_TRADES' && <div><TickerTradesTable data={TickerTradesTable} /> </div> } */}
 
     </div>
   );
--- src/components/RecentTradesTable.tsx
+++ src/components/RecentTradesTable.tsx
@@ -1,36 +1,80 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-interface Data {
+interface Trade {
   id: number;
   price: string;
   qty: string;
+  quoteQty: string;
+  time: number;
+  isBuyerMaker: boolean;
+  isBestMatch: boolean;
+}
+interface RecentTradesProps {
+  trades: Trade[];
 }
 
-interface RecentTradesTableProps {
-  data: Data[];
-}
+type SortField = 'time' | 'price' | 'qty';
 
-const RecentTradesTable: React.FC<RecentTradesTableProps> = ({ data }) => {
+const RecentTrades: React.FC<RecentTradesProps> = ({ trades }) => {
+  const [sortedTrades, setSortedTrades] = useState<Trade[]>([...trades]);
+  const [sortField, setSortField] = useState<SortField | null>(null);
+
+  const sortTrades = (field: SortField) => {
+    if (sortField === field) {
+      setSortedTrades([...sortedTrades.reverse()]);
+      return;
+    }
+  
+    const sorted = [...sortedTrades].sort((a, b) => {
+      let aValue: number, bValue: number;
+  
+      if (field === 'time') {
+        aValue = a[field];
+        bValue = b[field];
+      } else {
+        aValue = parseFloat(a[field]);
+        bValue = parseFloat(b[field]);
+      }
+  
+      return aValue - bValue;
+    });
+  
+    setSortedTrades(sorted);
+    setSortField(field);
+  };
+  
+
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>ID</th>
-          <th>Price</th>
-          <th>Quantity</th>
-        </tr>
-      </thead>
-      <tbody>
-        {data.map((data) => (
-          <tr key={data.id}>
-            <td>{data.id}</td>
-            <td>{data.price}</td>
-            <td>{data.qty}</td>
+    <div>
+      <h2>Recent Trades</h2>
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th onClick={() => sortTrades('price')}>Price</th>
+            <th onClick={() => sortTrades('qty')}>Quantity</th>
+            <th>Quote Quantity</th>
+            <th onClick={() => sortTrades('time')}>Time</th>
+            <th>Is Buyer Maker</th>
+            <th>Is Best Match</th>
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {sortedTrades.map((trade, index) => (
+            <tr key={index}>
+              <td>{trade.id}</td>
+              <td>{trade.price}</td>
+              <td>{trade.qty}</td>
+              <td>{trade.quoteQty}</td>
+              <td>{new Date(trade.time).toLocaleString()}</td>
+              <td>{trade.isBuyerMaker ? 'Yes' : 'No'}</td>
+              <td>{trade.isBestMatch ? 'Yes' : 'No'}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 };
 
-export default RecentTradesTable;
+export default RecentTrades;
